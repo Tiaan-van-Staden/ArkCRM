3d67f60d268b8086f3a51fcedff46456a2a1b3ed
Tiaan <tiaan.vanstaden@4-sure.net>
2025-08-18 17:53:30 +0200

    Add project files.

---
+++ ArkCRM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ArkCRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
