733a03f84479e9377e79b015f851aa4bed6ab302
Tiaan <tiaan.vanstaden@4-sure.net>
2025-08-18 18:40:41 +0200

    Initial commit - create Customer model and a View for testing

---
+++ ArkCRM/Controllers/CustomersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ArkCRM.Models;
+
+namespace ArkCRM.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult Overview()
+        {
+            var customer = new Customer() { CustomerName = "Tiaan", Address = "19 Harry Road", ContactNumber = 0812796647, Email = "tiaanvstaden@gmail.com" };
+            return View(customer);
+        }
+    }
+}
---
+++ ArkCRM/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace ArkCRM.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; } = "";
+        public string Address { get; set; } = "";
+        public int? ContactNumber { get; set; }
+        public string? Email { get; set; } = "";
+        public int? VatNumber { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
